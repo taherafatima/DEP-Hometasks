e7b2f4500a3eea0ee53059370b0470311132c8ad
taherafatima <80874045+taherafatima@users.noreply.github.com>
2021-05-05 17:43:07 +0530

    Add files via upload

---
+++ countingwords.js
@@ -0,0 +1,15 @@
+/*Can you realize a function that returns word count from a given string?
+
+You have to ensure that spaces in string is a whitespace for real.
+
+What we want and finish of work:
+
+countWords("Hello"); // returns 1 as int
+countWords("Hello, World!") // returns 2
+countWords("No results for search term `s`") // returns 6
+countWords(" Hello") // returns 1
+// ... and so on*/
+
+function countWords(str) {
+        return str.split(/[^\s]+/).length - 1;
+}
