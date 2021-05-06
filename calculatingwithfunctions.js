68fc774491bed29dd0b9862a4b1234758cdda999
taherafatima <80874045+taherafatima@users.noreply.github.com>
2021-05-06 16:44:44 +0530

    Update calculatingwithfunctions.js

--- calculatingwithfunctions.js
+++ calculatingwithfunctions.js
@@ -15,110 +15,110 @@ eight(dividedBy(three()));*/
 
 
 
-function zero() { 
-  if(arguments.length == 1){
-    return  arguments[0](0);
+function zero() {
+  if (arguments.length == 1) {
+    return arguments[0](0);
   }
   else {
-      return 0;
-    }
+    return 0;
+  }
 }
-  
-function one() { 
-    if(arguments.length == 1){
-    return  arguments[0](1);
+
+function one() {
+  if (arguments.length == 1) {
+    return arguments[0](1);
   }
   else {
-      return 1;
-    }
+    return 1;
+  }
 }
-function two() { 
-      if(arguments.length == 1){
-    return  arguments[0](2);
+function two() {
+  if (arguments.length == 1) {
+    return arguments[0](2);
   }
   else {
-      return 2;
-    }
+    return 2;
+  }
 }
-  
-function three() { 
-      if(arguments.length == 1){
-    return  arguments[0](3);
+
+function three() {
+  if (arguments.length == 1) {
+    return arguments[0](3);
   }
   else {
-      return 3;
-    }
+    return 3;
+  }
 }
 function four() {
-      if(arguments.length == 1){
-    return  arguments[0](4);
+  if (arguments.length == 1) {
+    return arguments[0](4);
   }
   else {
-      return 4;
-    }
+    return 4;
+  }
 }
-function five() { 
-      if(arguments.length == 1){
-    return  arguments[0](5);
+function five() {
+  if (arguments.length == 1) {
+    return arguments[0](5);
   }
   else {
-      return 5;
-    }
+    return 5;
+  }
 }
-function six() { 
-      if(arguments.length == 1){
-    return  arguments[0](6);
+function six() {
+  if (arguments.length == 1) {
+    return arguments[0](6);
   }
   else {
-      return 6;
-    }
+    return 6;
+  }
 }
-function seven() { 
-      if(arguments.length == 1){
-    return  arguments[0](7);
+function seven() {
+  if (arguments.length == 1) {
+    return arguments[0](7);
   }
   else {
-      return 7;
-    }
+    return 7;
+  }
 }
-function eight() { 
-      if(arguments.length == 1){
-    return  arguments[0](8);
+function eight() {
+  if (arguments.length == 1) {
+    return arguments[0](8);
   }
   else {
-      return 8;
-    }
+    return 8;
+  }
 }
-function nine() { 
-      if(arguments.length == 1){
-    return  arguments[0](9);
+function nine() {
+  if (arguments.length == 1) {
+    return arguments[0](9);
   }
   else {
-      return 9;
-    }
+    return 9;
+  }
 }
 
 function plus() {
-  var right = arguments[0]; 
-  return function(left) { 
-    return left + right; 
+  var right = arguments[0];
+  return function (left) {
+    return left + right;
   }
 }
 function minus() {
-  var right = arguments[0]; 
-  return function(left) { 
-    return left - right; 
+  var right = arguments[0];
+  return function (left) {
+    return left - right;
   }
 }
 function times() {
-  var right = arguments[0]; 
-  return function(left) { 
-    return left * right; 
+  var right = arguments[0];
+  return function (left) {
+    return left * right;
   }
 }
 function dividedBy() {
-  var right = arguments[0]; 
-  return function(left) { 
-    return Math.floor(left / right); 
+  var right = arguments[0];
+  return function (left) {
+    return Math.floor(left / right);
   }
 }
