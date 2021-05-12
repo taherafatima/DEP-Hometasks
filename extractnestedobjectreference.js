93209dd99ac0a2e1aa00c37cf7cb99d0912d2f3b
taherafatima <80874045+taherafatima@users.noreply.github.com>
2021-05-12 14:29:59 +0530

    Add files via upload

---
+++ extractnestedobjectreference.js
@@ -0,0 +1,33 @@
+/*You are given a complex object that has many deeply nested variables. You don't want to go the usual if obj.property == null route. Create a prototype method that given a nested path, either return the value or undefined.
+
+var obj = {
+  person: {
+    name: 'joe',
+    history: {
+      hometown: 'bratislava',
+      bio: {
+        funFact: 'I like fishing.'
+      }
+    }
+  }
+};
+
+obj.hash('person.name'); // 'joe'
+obj.hash('person.history.bio'); // { funFact: 'I like fishing.' }
+obj.hash('person.history.homeStreet'); // undefined
+obj.hash('person.animal.pet.needNoseAntEater'); // undefined
+*/
+
+// return the nested property value if it exists,
+// otherwise return undefined
+
+Object.prototype.hash = function (string) {
+  let property = `this.${string}`;
+  try {
+    return eval(property)
+  } catch (err) {
+    return undefined;
+  }
+
+
+}
